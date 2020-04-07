547fc90bb5ced44fe302f260757b0d8ce9a655f4
katerynaprokofieva <gucolk@gmail.com>
2020-04-07 22:39:46 +0300

    add more functionality

--- src/script.js
+++ src/script.js
@@ -3,9 +3,9 @@ const firstRow = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=
 const secondRow = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'];
 const thirdRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''];
 const fourthRow = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'];
-const secondRowRus = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ё'];
-const thirdRowRus = ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'];
-const fourthRowRus = ['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/'];
+// const secondRowRus = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ё'];
+// const thirdRowRus = ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'];
+// const fourthRowRus = ['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/'];
 const deleteButton = 'delete';
 const tabButton = 'tab';
 const capsButton = 'caps lock';
@@ -18,8 +18,6 @@ const topArrowButton = '&uarr;';
 const bottomArrowButton = '&darr;';
 const rightArrowButton = '&rarr;';
 const leftArrowButton = '&larr;';
-const textArea = document.createElement('textarea');
-
 
 const createNewDiv = (className, btnClassName, element) => {
   const div = document.createElement('div');
@@ -35,6 +33,7 @@ div.className = 'wrapper';
 document.body.append(div);
 
 // text area
+const textArea = document.createElement('textarea');
 textArea.className = 'text-area';
 document.querySelector('.wrapper').append(textArea);
 
@@ -42,7 +41,6 @@ document.querySelector('.wrapper').append(textArea);
 firstRow.forEach((element) => {
   createNewDiv('button', 'btn', element);
 });
-
 createNewDiv('backspace-button', 'btn', deleteButton);
 
 // second row english
@@ -98,7 +96,7 @@ const setTimer = (element) => {
   return setTimeout(() => element.style.borderRadius = '', 500);
 };
 
-const handleCapsLock = (startOfRow, endOfRow, elem) => {
+const handleCapsLock = (startOfRow, endOfRow) => {
   for (let i = startOfRow; i < endOfRow; i++) {
     const a = Array.from(document.querySelector('.wrapper').querySelectorAll('.button'))[i];
     if (a.firstElementChild.style.textTransform === 'capitalize') {
@@ -136,46 +134,73 @@ document.querySelector('.wrapper').addEventListener('click', (event) => {
     handleCapsLock(37, 44);
   } else if (backspaceBtn && (backspaceBtn.firstElementChild.innerHTML === tabButton)) {
     textArea.value += '    ';
-  }
-   else if (arrowButton) {
-    if(arrowButton.firstElementChild.innerText === "←") {
+  } else if (arrowButton) {
+    if (arrowButton.firstElementChild.innerText === '←') {
       textArea.value += arrowButton.firstElementChild.innerText;
-  } else if (arrowButton.firstElementChild.innerText === "↓") {
+    } else if (arrowButton.firstElementChild.innerText === '↓') {
       textArea.value += arrowButton.firstElementChild.innerText;
-  } else if (arrowButton.firstElementChild.innerText === "→") {
-    textArea.value += arrowButton.firstElementChild.innerText;
-  } else if (arrowButton.firstElementChild.innerText === "↑") {
-    textArea.value += arrowButton.firstElementChild.innerText;
-  }
+    } else if (arrowButton.firstElementChild.innerText === '→') {
+      textArea.value += arrowButton.firstElementChild.innerText;
+    } else if (arrowButton.firstElementChild.innerText === '↑') {
+      textArea.value += arrowButton.firstElementChild.innerText;
+    }
   }
 });
 
-document.addEventListener('keydown', (event) => {
+const animationForSpecialBtns = (arrayElem) => {
+  Array.from(document.querySelectorAll('.btn'))[arrayElem].style.backgroundColor = 'pink';
+  Array.from(document.querySelectorAll('.btn'))[arrayElem].style.borderRadius = '20px';
+};
 
+document.addEventListener('keydown', (event) => {
   document.querySelectorAll('.btn').forEach((el) => {
-    if (el.firstElementChild.innerHTML === event.key) {
+    if (el.firstElementChild.innerHTML === event.key && el.firstElementChild.style.textTransform === 'capitalize') {
+      el.style.backgroundColor = 'pink';
+      el.style.borderRadius = '20px';
+      textArea.value += event.code.substr(-1);
+    } else if (el.firstElementChild.innerHTML === event.key) {
       el.style.backgroundColor = 'pink';
       el.style.borderRadius = '20px';
       textArea.value += event.key;
-    } 
+    }
   });
 
   if (event.keyCode === 8) {
-    Array.from(document.querySelectorAll('.btn'))[13].style.backgroundColor = 'pink';
-    Array.from(document.querySelectorAll('.btn'))[13].style.borderRadius = '20px';
+    animationForSpecialBtns(13);
     textArea.value = textArea.value.slice(0, -1);
+  } else if (event.keyCode === 13) {
+    animationForSpecialBtns(40);
+    textArea.value += '\n';
+  } else if (event.keyCode === 32) {
+    animationForSpecialBtns(57);
+    textArea.value += ' ';
+  } else if (event.keyCode === 37) {
+    animationForSpecialBtns(59);
+    textArea.value += '←';
+  } else if (event.keyCode === 38) {
+    animationForSpecialBtns(52);
+    textArea.value += '↑';
+  } else if (event.keyCode === 39) {
+    animationForSpecialBtns(61);
+    textArea.value += '→';
+  } else if (event.keyCode === 40) {
+    animationForSpecialBtns(60);
+    textArea.value += '↓';
+  } else if (event.keyCode === 9) {
+    animationForSpecialBtns(14);
+    textArea.value += '    ';
+    event.preventDefault();
+  } else if (event.keyCode === 20) {
+    animationForSpecialBtns(28);
+    handleCapsLock(13, 23);
+    handleCapsLock(26, 37);
+    handleCapsLock(37, 44);
   }
 });
 
-document.addEventListener('keyup', (event) => {
+document.addEventListener('keyup', () => {
   document.querySelectorAll('.btn').forEach((el) => {
-    if (el.firstElementChild.innerHTML === event.key) {
-      el.style.backgroundColor = '';
-      el.style.borderRadius = '';
-    } else if (event.keyCode === 8) {
-      Array.from(document.querySelectorAll('.btn'))[13].style.backgroundColor = '';
-      Array.from(document.querySelectorAll('.btn'))[13].style.borderRadius = '';
-    }
-
+    el.style.backgroundColor = '';
+    el.style.borderRadius = '';
   });
 });
